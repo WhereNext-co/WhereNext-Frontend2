d78b7a03dcf1de32b75714babbee41c20197b236
mearzwong <mearz99@hotmail.com>
2024-05-09 14:42:06 +0700

    last update from presentation day

--- src/app/(sign_up)/Introduce.js
+++ src/app/(sign_up)/Introduce.js
@@ -1,46 +1,58 @@
-import { View, Text, TextInput,  StyleSheet,Pressable ,Linking } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Pressable,
+  Linking,
+} from "react-native";
 import { Link } from "expo-router";
-import React  from "react";
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { useNavigation } from '@react-navigation/native';
-import { router,Stack } from "expo-router";
+import React from "react";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from "@react-navigation/native";
+import { router, Stack } from "expo-router";
 
-import SlideButton from '../../components/componentspung/Button/SlideButton/Button';
-
+import SlideButton from "../../components/componentspung/Button/SlideButton/Button";
 
 export default function Login() {
-
   const handlePress = () => {
-    router.push('/Name')
+    router.push("/Name");
   };
   const handlePress2 = () => {
-    router.push('/sign-in')
+    router.push("/sign-in");
   };
-  
-  return(
+
+  return (
     <View style={styles.container}>
       <Stack.Screen options={{ headerShown: false }} />
-      <SlideButton label={"Introduce your self "} onPress={handlePress}></SlideButton>
-      
-      
-        <Pressable style={{}} onPress={handlePress2}>
-        <Text style={{textAlign :"center",
-        marginTop:15, 
-        textAlignVertical:"center",
-        fontSize:15, 
-        color:'white'}}>
-        Already have an account? login in <Icon name="arrow-right" size={11} color="white" /></Text>
-        </Pressable>
-      
+      <SlideButton
+        label={"Introduce yourself "}
+        onPress={handlePress}
+      ></SlideButton>
+
+      <Pressable style={{}} onPress={handlePress2}>
+        <Text
+          style={{
+            textAlign: "center",
+            marginTop: 15,
+            textAlignVertical: "center",
+            fontSize: 15,
+            color: "white",
+          }}
+        >
+          Already have an account? login in{" "}
+          <Icon name="arrow-right" size={11} color="white" />
+        </Text>
+      </Pressable>
     </View>
-    ); 
+  );
 }
 const styles = StyleSheet.create({
-    // Styles that are unchanged from previous step are hidden for brevity. 
-    container: {
-      flex: 1,
-      backgroundColor: '#14072b',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }
-  });
+  // Styles that are unchanged from previous step are hidden for brevity.
+  container: {
+    flex: 1,
+    backgroundColor: "#14072b",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
